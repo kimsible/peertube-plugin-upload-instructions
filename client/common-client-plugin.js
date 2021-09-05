3014425e46d6da12e24f6de77027a5aa195599e2
Ms Kimsible <kimsible@users.noreply.github.com>
2021-09-05 22:19:03 +0200

    Feat: show instructions in a modal

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -2,19 +2,37 @@ export {
   register
 }
 
-async function register ({ registerHook }) {
+async function register ({ registerHook, peertubeHelpers }) {
+  const settings = await peertubeHelpers.getSettings()
+
+  const showInstructions = async () => {
+    const { instructionsStatus, instructionsBody } = settings
+
+    if (instructionsStatus) {
+      const { markdownRenderer, showModal } = peertubeHelpers
+
+      showModal({
+        title: 'Upload instructions',
+        content: await markdownRenderer.enhancedMarkdownToHTML(instructionsBody),
+        confirm: {
+          value: 'OK'
+        }
+      })
+    }
+  }
+
   // Run when route is /videos/upload
   registerHook({
     target: 'action:router.navigation-end',
     handler: ({ path }) => {
       if (path === '/videos/upload') {
-        // HANDLE HERE
+        showInstructions()
       }
     }
   })
 
   // Run when refresh or manually enter /videos/upload route in browser
   if (window.location.pathname === '/videos/upload') {
-    // HANDLE HERE
+    showInstructions()
   }
 }
