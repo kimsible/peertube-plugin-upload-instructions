436ec79cc26993ddf408ed6d5476afcbf0e713ce
Ms Kimsible <kimsible@users.noreply.github.com>
2021-09-05 21:51:47 +0200

    Feat: add plugin settings fields

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-async function register () {}
+async function register ({ registerSetting }) {
+  registerSetting({
+    name: 'instructionsStatus',
+    label: 'Enable instructions modal',
+    type: 'input-checkbox',
+    private: false
+  })
+
+  registerSetting({
+    name: 'instructionsBody',
+    label: 'Instructions body',
+    type: 'markdown-enhanced',
+    private: false
+  })
+}
 async function unregister () {}
 
 module.exports = {
